993f7b40ff67abcc6f46709cdc37cb64bcf18078
SnowmanYP <ngfax@mail.ru>
2024-12-20 18:28:20 +0300

    Спринт 9/18: 9 → Тема 3/5: Работа с JSON → Урок 4/5
    Десериализуем коллекцию объектов и создаём редактор субтитров

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,34 +1,59 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-
-static enum SubtitleLanguage {en,cn,ru}
-
+enum SubtitleLanguage {en,cn,ru}
 
 class SubtitleItem {
-    Map values = new HashMap<>();
-
-    LocalTime begin;
-
-    LocalTime end;
+    private Map values;
+    private LocalTime begin;
+    private LocalTime end;
 
     // геттеры и сеттеры
 
+    public Map getValues() {
+        return values;
+    }
+
+    public void setValues(Map values) {
+        this.values = values;
+    }
+
+    public LocalTime getBegin() {
+        return begin;
+    }
+
+    public void setBegin(LocalTime begin) {
+        this.begin = begin;
+    }
+
+    public LocalTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalTime end) {
+        this.end = end;
+    }
+
     @Override
     public boolean equals(Object o) {
-       ...
+        // ...
+        return false; //--
     }
 
     @Override
     public int hashCode() {
-       ...
+      // ...
+        return 0; //--
     }
-
 
     public SubtitleItem(Map values, LocalTime begin, LocalTime end) {
         this.values = values;
@@ -37,26 +62,29 @@ class SubtitleItem {
     }
 }
 
-class SubtitleListTypeToken extends ??? {
-
-        }
-
-class LocalTimeTypeAdapter extends ??? {
-private final DateTimeFormatter timeFormatter = ???
-
-@Override
+class SubtitleListTypeToken extends TypeToken<List<SubtitleItem>> //<>??
+ { //Здесь ничего писать не нужно
+             }
+class LocalTimeTypeAdapter extends TypeAdapter<SubtitleItem> //<>??
+ {
+private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.SS");
+/*@Override
 public void write(???) throws IOException {
-
-}
+}*/
+/*@Override
+public LocalTime read(???) throws IOException {
+}*/
 
 @Override
-public LocalTime read(???) throws IOException {
+public void write(JsonWriter out, SubtitleItem value) throws IOException {
+     }
 
-}
+@Override
+public SubtitleItem read(JsonReader in) throws IOException
+{ return null; }
 }
 
 public  class Practicum {
-
     public static void main(String[] args) {
         List<SubtitleItem> subtitles = Arrays.asList(
                 new SubtitleItem(Map.of(SubtitleLanguage.ru, "Здравствуйте!",
@@ -84,18 +112,17 @@ public  class Practicum {
                         LocalTime.of(0, 0, 37)
                 )
         );
-
         // адаптер для преобразования типа LocalTime в String в формате субтитров
-        ??? localTimeTypeAdapter = ???
+        LocalTimeTypeAdapter localTimeTypeAdapter =new LocalTimeTypeAdapter();
 
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.???
-        Gson gson = gsonBuilder.???
+        gsonBuilder.registerTypeAdapter(LocalTime.class,localTimeTypeAdapter);
+        Gson gson = gsonBuilder.create(); //???+++
 
-        String subtitlesJson = gson.toJson(???);
+        String subtitlesJson = gson.toJson(subtitles.toArray().toString());
         System.out.println(subtitlesJson);
 
-        List<SubtitleItem> parsed = gson.fromJson(???, ???);
+        List<SubtitleItem> parsed = gson.fromJson(subtitlesJson,new SubtitleListTypeToken()); //+++
         if(parsed.equals(subtitles)) {
             System.out.println("Субтитры десериализованы корректно.");
         } else {
